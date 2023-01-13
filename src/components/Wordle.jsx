10a651fc907d7a45a3b8761335b05eb9446ba762
Neil Hanson <pro.neilhanson@gmail.com>
2023-01-13 15:38:50 -0600

    finished mvp of worder clone

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -1,23 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import useWordle from "../hooks/useWorlde";
 import Grid from "./Grid";
 import Keypad from "./Keypad";
 import keys from "../constants/keys";
+import Modal from "./Modal";
 
 export default function Wordle({ solution }) {
     const { currentGuess, guesses, turn, isCorrect, usedKeys, handleKeyup } = useWordle(solution);
 
+    const [showModal, setShowModal] = useState(false)
+
     useEffect(() => {
         window.addEventListener('keyup', handleKeyup);
 
+        if (isCorrect) {
+            setTimeout(() => setShowModal(true), 2000)
+            window.removeEventListener('keyup', handleKeyup)
+        }
+        if (turn > 5) {
+            window.removeEventListener('keyup', handleKeyup)
+            setTimeout(() => setShowModal(true), 2000)
+        }
+
         return () => window.removeEventListener('keyup', handleKeyup);
-    }, [handleKeyup]);
+    }, [handleKeyup, isCorrect, turn]);
 
     return (
         <div>
             <div>Current Guess - {currentGuess}</div>
             <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
             <Keypad keys={keys} usedKeys={usedKeys}/>
+            {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
         </div>
     )
 }
